97c27d9b5fd21e5e1e69b79268e5ecf6441eafd7
Ivan Grishaev <ivan@grishaev.me>
2020-09-22 10:00:02 +0300

    cleanup

--- src/clojurernproject/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns clojurernproject.core
-  (:require [steroid.rn.core :as rn]))
-
-(defn root-comp []
-  [rn/view
-    [rn/text "Hello CLojure! from CLJS"]])
-
-(defn init []
-  (rn/register-reload-comp "ClojureRNProject" root-comp))
